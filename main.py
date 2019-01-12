06be59f1bc3b899489aa48efce4aea9ae624b7ed
milochadam <miloch.adam@gmail.com>
2019-01-12 16:30:33 +0100

    Added category class, removed unused classes

--- main.py
+++ main.py
@@ -5,69 +5,13 @@ import random
 import os
 
 
-class RANDOM_ATTR(EnumMeta):
-    @property
-    def RANDOM(self):
-        return random.choice([Dict.german, Dict.polish]).value
+def parse_german(word):
+    return word.replace('ß', 'ss').replace('ü', 'ue').replace('ö', oe).replace('ä', ae)
 
 
-class Dict(Enum, metaclass=RANDOM_ATTR):
-    polish = 0
-    german = 1
+class CouldNotLoadDatabaseException(Exception):
+    pass
 
-
-class Database:
-    word_bank_dir = 'slownictwo'
-    dictionary = {}
-    count = 0
-
-    def load_database(self):
-        if os.path.isdir(self.word_bank_dir):
-            for filename in os.listdir(self.word_bank_dir):
-                with open(os.path.join(self.word_bank_dir, filename), 'r') as f:
-                    title = f.readline().strip('\n')
-                    self.dictionary[filename] = {
-                        'title': title,
-                        'value': []
-                    }
-                    for line in f:
-                        split_line = [x.strip(' ') for x in line.strip('\n').split('-')]
-                        self.dictionary[filename]['value'].append(
-                            Question(*split_line))
-                        self.count += 1
-        else:
-            os.makedirs(self.word_bank_dir)
-            print('W katalogu "slownictwo" nie znaleziono żadnego pliku ze słownictwem.')
-            print('Stwórz plik i spróbuj jeszcze raz.')
-
-    def get_size(self):
-        return self.count
-
-    def get_random_questions(self, prefs, size=10):
-        # TODO: validate number is le count of chosen categories
-        if size > self.count:
-            size = self.count
-
-        all_words = []
-        for unit_name in prefs:
-            words = self.dictionary[unit_name]['value']
-            all_words += words
-
-        return random.sample(all_words, size)
-
-class Question:
-    def __init__(self, question, answer):
-        self.question = question
-        self.answer = answer
-
-    def get_question(self):
-        return self.question
-
-    def get_answer(self):
-        return self.answer
-
-    def invert(self):
-        return Question(self.answer, self.question)
 
 class Points:
     def __init__(self):
@@ -100,26 +44,58 @@ class Program:
         self.db = Database()
 
     def run(self):
-        self.db.load_database()
-        preferences = self.get_test_preferences()
-        test = self.db.get_random_questions(preferences, 10)
-        self.solve_test(test)
-        self.show_results()
+        try:
+            self.db.load_database()
+            preferences = self.get_test_preferences()
+            test = self.get_random_questions(preferences, 10)
+            self.solve_test(test)
+            self.show_results()
+
+        except CouldNotLoadDatabaseException:
+            print('W katalogu "slownictwo" nie znaleziono żadnego pliku ze słownictwem.')
+            print('Stwórz plik i spróbuj jeszcze raz.')
 
         if os.name == 'nt':
             os.system('pause')
 
-    def show_results(self):
-        print()
-        print('=== WYNIKI ===')
-        print(self.points)
+    def get_test_preferences(self):
+        all_categories = self.db.get_categories()
+        chosen_categories = []
+
+        print('Dostępne kategorie: ')
+        for i, cat in enumerate(all_categories):
+            print('   {}. {}'.format(i, cat.get_title()))
+
+        while True:
+            ans = input('Czy chcesz test z wszystkich kategorii? [y/n]')
+            if ans not in ('y', 'n'):
+                continue
+            if ans == 'y':
+                return all_categories
+            if ans == 'n':
+                break
+
+        print('\nWybierz kategorie:')
+        for cat in all_categories:
+            ans = input(cat.get_title() + '? [y/n]')
+            if ans == 'y' or ans == '':
+                chosen_categories.append(cat)
+
+        return chosen_categories
+
+    def get_random_questions(self, categories, size=10):
+        questions = []
+        for c in categories:
+            questions += c.get_questions()
+
+        if size > len(questions):
+            size = len(questions)
+
+        return random.sample(questions, size)
 
     def solve_test(self, test):
         for test_question in test:
             try:
-
-                #from_lang = Dict.RANDOM
-                #to_lang = Dict.german.value if from_lang == Dict.polish.value else Dict.polish.value
                 from_lang = 0
                 to_lang = 1
 
@@ -134,30 +110,79 @@ class Program:
                     self.points.bad_ans()
             except KeyboardInterrupt:
                 return
+            except EOFError:
+                return
 
-    def get_test_preferences(self):
-        all_categories = list(self.db.dictionary.keys())
-        print('Dostępne kategorie: ')
-        for i,cat in enumerate(all_categories):
-            print('   {}'.format(i), self.db.dictionary[cat]['title'])
+    def show_results(self):
+        print()
+        print('=== WYNIKI ===')
+        print(self.points)
 
-        while True:
-            ans = input('Czy chcesz test z wszystkich kategorii? [y/n]')
-            if ans in ('y', 'n'):
-                break
 
-        if ans == 'y':
-            return all_categories
+class Database:
+    word_bank_dir = 'slownictwo'
+    categories = []
+    count = 0
 
-        chosen_categories = []
-        # Wybierz kategorie
-        print('\nWybierz kategorie:')
-        for cat in all_categories:
-            ans = input(self.db.dictionary[cat]['title'] + '? [y/n]')
-            if ans == 'y' or ans == '':
-                chosen_categories.append(cat)
+    def load_database(self):
+        if os.path.isdir(self.word_bank_dir):
+            for filename in os.listdir(self.word_bank_dir):
+                with open(os.path.join(self.word_bank_dir, filename), 'r') as f:
+                    title = f.readline().strip('\n')
+                    cat = Category(filename, title)
+                    for line in f:
+                        split_line = [x.strip(' ')
+                                      for x in line.strip('\n').split('-')]
+                        cat.add_question(Question(*split_line))
+                        self.count += 1
+                    self.categories.append(cat)
+        else:
+            os.makedirs(self.word_bank_dir)
+            raise CouldNotLoadDatabaseException
 
-        return chosen_categories
+    def get_categories(self):
+        return self.categories
+
+    def get_size(self):
+        return self.count
+
+
+class Category:
+    def __init__(self, filename, title):
+        self.filename = filename
+        self.title = title
+        self.questions = []
+
+    def get_filename(self):
+        return self.filename
+
+    def get_title(self):
+        return self.title
+
+    def add_question(self, question):
+        self.questions.append(question)
+
+    def get_questions(self):
+        return self.questions
+
+
+class Question:
+    def __init__(self, question, answer, dest='niemiecki'):
+        self.question = question
+        self.answer = answer
+        self.dest = dest
+
+    def get_question(self):
+        return self.question
+
+    def get_answer(self):
+        return self.answer
+
+    def dest_lang(self):
+        return self.dest
+
+    def invert(self):
+        return Question(self.answer, self.question)
 
 
 if __name__ == "__main__":
