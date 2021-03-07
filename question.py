144e5f39d4ec5d1155c74d982ff61862af486178
milochadam <miloch.adam@gmail.com>
2021-03-07 20:04:35 +0100

    Skip parens in answers

--- question.py
+++ question.py
@@ -1,12 +1,11 @@
 from __future__ import annotations
 
+import re
 from typing import Any
 from typing import List
 from typing import Optional
 
 
-# TODO(#19): Question should be renamed to Flashcard
-# TODO(#20): Implement test suite
 class Question:
     def __init__(self, question: List[str], answers: List[str], category: Optional[str] = None, **kwargs: Any) -> None:
         assert len(answers) > 0
@@ -33,7 +32,7 @@ class Question:
             return self.question
 
     def is_correct(self, answer: str) -> bool:
-        return answer in self._answers
+        return answer in [self.get_parenless(answer) for answer in self._answers]
 
     def __str__(self) -> str:
         return f'{self.question} - {self.answers}'
@@ -46,3 +45,7 @@ class Question:
 
     def reversed(self) -> 'Question':
         return Question(question=self._answers, answers=self._question, category=self._category)
+
+    @staticmethod
+    def get_parenless(expression: str) -> str:
+        return re.sub(' ?\(\w+\)', '', expression)
